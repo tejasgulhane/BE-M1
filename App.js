d12d446adf9fa76baadbc761e5fef58fbb05b58b
tejasgulhane <96588203+tejasgulhane@users.noreply.github.com>
2022-12-04 15:20:36 +0530

    BE-M1-T7

--- App.js
+++ App.js
@@ -1,37 +1,45 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
+// const { log } = require('console');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Tejas</h1>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/home") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome to Home</h1>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome to About</h1>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/node") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<h1>Welcome to Node Server</h1>");
-    res.write("</html>");
+  const method = req.method;
+
+  if (url === '/') {
+    console.log("inside");
+
+    fs.readFile('message.txt', (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+    
+        console.log( data )
+        res.write('<html>');
+        res.write(`<h1>${data}</h1>`);
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    })
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<h1>Tejas</h1>");
-  res.write("</html>");
-  res.end();
+
 });
 
-server.listen(4000);
+server.listen(4001);
+
+
