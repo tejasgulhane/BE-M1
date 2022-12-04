9cb1a27f005e4c61d74a0723cfeb00add4f2113d
tejasgulhane <96588203+tejasgulhane@users.noreply.github.com>
2022-12-04 13:34:51 +0530

    BE-M1-T6

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res) =>{
-console.log(req.method , req.url, req.headers)
-
-res.setHeader('Content-Type', 'text/html')
-res.write('<html>')
-res.write('<h1>Tejas</h1>')
-res.write('</html>')
-res.end();
-
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Tejas</h1>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome to Home</h1>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome to About</h1>");
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>Welcome to Node Server</h1>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<h1>Tejas</h1>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000)
-
+server.listen(4000);
