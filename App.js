25bcada5830c7b385c0936c8b68431872dd9d9ab
tejasgulhane <96588203+tejasgulhane@users.noreply.github.com>
2022-12-05 09:44:03 +0530

    BE-M1-T8

--- App.js
+++ App.js
@@ -1,44 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-// const { log } = require('console');
+const routes = require('./Routes');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    console.log("inside");
-
-    fs.readFile('message.txt', (err, data) => {
-        if (err) {
-            console.log(err)
-        }
-    
-        console.log( data )
-        res.write('<html>');
-        res.write(`<h1>${data}</h1>`);
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    })
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-
-});
+const server = http.createServer(routes);
 
 server.listen(4001);
 
